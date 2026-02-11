eb9e1c83ae45eebb4f2cd051d7b7b61ac784a05a
sohammarathe2004 <2025.soham.marathe@ves.ac.in>
2026-02-11 15:10:10 +0530

    Update ReadFromFile.java

--- ReadFromFile.java
+++ ReadFromFile.java
@@ -1,10 +1,12 @@
 import java.io.*;
 public class ReadFromFile {
     public static void main(String[] args) {
+        try{
        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
 int a = Integer.parseInt(br.readLine());
 int b = Integer.parseInt(br.readLine());
 br.close()
   System.out.println("Sum:"+(a+b));
-    }
+    }}
+    catch(Exception e){System.out.println("Error:"+e.getMessage();}
 }
